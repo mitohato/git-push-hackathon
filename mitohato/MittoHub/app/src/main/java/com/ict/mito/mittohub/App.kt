c599d20872216a3b6ad9c0876412ffbd2cbe64af
mitohato <mitohato14@gmail.com>
2018-10-15 06:20:18 +0900

    Impl App

--- mitohato/MittoHub/app/src/main/java/com/ict/mito/mittohub/App.kt
+++ mitohato/MittoHub/app/src/main/java/com/ict/mito/mittohub/App.kt
@@ -1,7 +1,23 @@
 package com.ict.mito.mittohub
 
+import androidx.multidex.MultiDexApplication
+import org.koin.android.ext.android.startKoin
+import org.koin.dsl.module.Module
+import org.koin.dsl.module.module
+
 /**
  * Created by mito on 2018/10/14.
  */
-class App {
+class App : MultiDexApplication() {
+    
+    override fun onCreate() {
+        super.onCreate()
+        startKoin(
+                this,
+                listOf(module)
+        )
+    }
+    
+    private val module: Module = module {
+    }
 }
